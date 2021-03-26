b4d41011bd2b0d5bca04d4be994047910c2de8ae
Никита Сидоренко <nikitastar280@gmail.com>
2021-03-26 16:59:41 +0300

    init

---
+++ Flavius-Josephus-Task/Flavius/FlaviusJosephus.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flavius
+{
+    /// <summary>
+    /// The class provides methods for crossing out each k person's qualifier in sequence.
+    /// </summary>
+    public class FlaviusJosephus
+    {
+        private List<int> people;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FlaviusJosephus"/> class.
+        /// </summary>
+        /// <param name="count">People count.</param>
+        public FlaviusJosephus(int count)
+        {
+            if (count < 1)
+            {
+                throw new ArgumentException($"{nameof(count)} less than one");
+            }
+
+            this.people = new List<int>(count);
+            for (int i = 0; i < count; i++)
+            {
+                this.people.Add(i + 1);
+            }
+        }
+
+        /// <summary>
+        /// Crossing out each k person's qualifier in sequence.
+        /// </summary>
+        /// <param name="k">Position.</param>
+        /// <returns>People.</returns>
+        public IEnumerable<int> Effacement(int k)
+        {
+            int current = 0;
+            int counter = 0;
+            while (this.people.Count != 0)
+            {
+                if (current >= this.people.Count)
+                {
+                    current = 0;
+                }
+
+                if (counter == k - 1)
+                {
+                    yield return this.people[current];
+                    this.people.RemoveAt(current);
+                    counter = 0;
+                }
+
+                counter++;
+                current++;
+            }
+        }
+    }
+}
---
+++ Flavius-Josephus-Task/FlaviusJosephusTests/FlaviusJosephusTests.cs
@@ -0,0 +1,26 @@
+﻿using Flavius;
+using FlaviusJosephusTests;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace FlaviusJosephusTests
+{
+    public class FlaviusJosephusTests
+    {
+        [TestCase(20, 3, 20)]
+        [TestCase(25, 2, 15)]
+        [TestCase(3, 45, 1)]
+        [TestCase(123, 45, 33)]
+        public void СheckingCorrectTower(int n, int k, int last)
+        {
+            var flaviusJosephus = new FlaviusJosephus(n);
+            var actual = flaviusJosephus.Effacement(k);
+            Assert.AreEqual(actual.Last(), last);
+        }
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        public void NLessTnaOne(int n) => Assert.Throws<ArgumentException>(() => new FlaviusJosephus(n));
+    }
+}
